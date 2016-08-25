b9c6ac44e108a15d8e704d4832241bc7f5f416e2
xlx <xlx@braun>
2016-08-25 23:50:31 +1000

    rename scripts

--- batch_confs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-array=( ASPLOS ISCA MICRO INFOCOM NSDI PODS)
-for i in "${array[@]}"
-do
-	python export_citations.py $i
-	python construct_citation_table.py $i
-done
--- batch_confs2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-array=( CCS HPDC ICS MOBICOM MobiSys SenSys EuroSys )
-for i in "${array[@]}"
-do
-	python export_citations.py $i
-	python construct_citation_table.py $i
-done
